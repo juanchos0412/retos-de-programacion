a44280ce51e034448543f0baa8e1a54b14447e07
jvalderramaduenas <jvalderramaduenas@gmail.com>
2024-07-10 03:24:05 +0000

    acualización primer ejercicio

--- javascript/00.js
+++ javascript/00.js
@@ -1,1 +1,27 @@
-console.log("hola")
+//Sitio web JavaScript https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una sola linea
+
+/* 
+
+Comentario de varias lineas
+
+*/
+var variableone = "primera variable"
+const variabletwo = "segunda variable"
+
+// tipos de variables
+
+let string = "este es un estring (cadena de texto)"
+let number = 0
+let booleano = true
+let array = [1, 2, 3, 4, 5, 'bob', true, false]
+let object = document.querySelector('h1')
+
+console.log('¡Hola, JavaScipt!')
+
+console.log(typeof string)
+console.log(typeof number)
+console.log(typeof booleano)
+console.log(typeof array)
+console.log(typeof object)
---
+++ php/00.php
@@ -0,0 +1,11 @@
+//Sitio web JavaScript https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//Comentario de una sola linea
+
+/* 
+
+Comentario de varias lineas
+
+*/
+
+$variable = "Esta es la primera variable"
